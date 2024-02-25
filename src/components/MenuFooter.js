b11ce45b2a6b13b7be55b7b0287c847dd5894ab3
sizeru <gaafts@gmail.com>
2024-02-25 11:29:53 -0600

    Add $ to total

--- src/components/MenuFooter.js
+++ src/components/MenuFooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 const MenuFooter = ({total, onReset, onOrder}) => {
     return (
         <div className='menu-footer'>
-            <p>TOTAL: {total.toFixed(2)}</p>
+            <p>TOTAL: ${total.toFixed(2)}</p>
             <div className='menu-footer-buttons'>
                 <button className='menu-item-button' onClick={onOrder}>Order</button>
                 <button className='menu-item-button' onClick={onReset}>Clear All</button>
